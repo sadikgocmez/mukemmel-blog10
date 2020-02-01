cba05f3f6c31ce6c86c0d360b6fb31a18d729f7d
sadikgocmez <59099015+sadikgocmez@users.noreply.github.com>
2020-02-01 13:46:23 +0300

    card

--- pages/[postId].js
+++ pages/[postId].js
@@ -85,13 +85,7 @@ const BlogPost = ({ post }) => (
             </div>
           </div>
           <br></br>
-          <div className="card" styleName="width: 18rem;">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-            </div>
-          </div>
+         
 
         </div>
 
--- pages/index.js
+++ pages/index.js
@@ -129,15 +129,7 @@ const Home = ({ posts }) => (
             </div>
           </div>
           <br></br>
-          <div className="card" styleName="width: 18rem;">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-            </div>
-          </div>
-
-        </div>
+                </div>
 
       </div>
 
